c03a46fea552fa1656a6cc650d49ab62e5befcfe
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-02-12 13:54:49 +0700

    Enhance-AddReceiptMaterial-ProductCompotition

---
+++ com.ambassador.support.lib/Services/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace com.ambassador.support.lib.Services
+{
+    public class IdentityService
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public int TimezoneOffset { get; set; } = 7;
+    }
+}
